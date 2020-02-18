db9982a52ba0e87320f542a425a80380c276da3f
Aimar <menoyakov.ai@gmail.com>
2020-02-17 19:45:28 -0500

    Making one-question sat word test

--- satwords.py
+++ satwords.py
@@ -7,15 +7,11 @@ with open("words.txt") as f:
     soup = BeautifulSoup(f, "lxml")
     trs = soup.find_all("tr")
     for tr in trs:
-        word = {}
+        word = ""
         for i, child in enumerate(tr.children):
-            if i == 3:
-                word["word"] = ""
-                word["word"] = child.string
-            elif i == 5:
-                word["meaning"] = ""
-                word["meaning"] = child.string
+            if i == 5:
+                word = child.string
         words.append(word)
 
-with open("words.json", "w+") as f:
+with open("meanings.txt", "w+") as f:
     json.dump(words, f)
