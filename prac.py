ad846c0fd8e904f425b88f81a8db0db345a990ed
Aimar <menoyakov.ai@gmail.com>
2020-02-19 23:25:19 -0500

    WORD OF THE DAY IS WORKIIINGG YAYAYAYAYAYYAYAYAYAY

--- prac.py
+++ prac.py
@@ -1,11 +1,15 @@
 import telebot
 from telebot import types
 import json
+from multiprocessing import Process, Queue
 import random
+import emoji
+from sender import start_sender, get_current_seconds
 
-bot = telebot.TeleBot("881872217:AAHjDOnnKDXr5D5LnyphqkO7Roe4QboXhTA")
+TOKEN = "881872217:AAHjDOnnKDXr5D5LnyphqkO7Roe4QboXhTA"
+bot = telebot.TeleBot(TOKEN)
 
-chat = []
+queue = Queue(maxsize=100)
 
 
 def open_json(s):
@@ -21,7 +25,6 @@ bookslist = open_json("books.json")
 # Нужно переделать
 booktextlist = {}
 for i, book in enumerate(bookslist):
-    booktextlist[book["text"]] = {}
     booktextlist[book["text"]] = i
 # ----------------
 
@@ -30,50 +33,60 @@ officialstextlist = {}
 
 # Как и это
 for i, official in enumerate(officialslist):
-    officialstextlist[official["text"]] = {}
     officialstextlist[official["text"]] = i
 # ---------
 
-imageslist = []
-buttons_list = open_json("buttons.json")
+funcs = open_json("buttons.json")
 
 
-def menu(id):
-    markup = types.InlineKeyboardMarkup()
-    # Берем кнопки из json файла buttons.json
+def menu(chat_id):
+    markup = types.ReplyKeyboardMarkup()
     btns = list(
         map(
-            lambda x: types.InlineKeyboardButton(
-                text=x["text"],
-                callback_data=x["callback_data"]),
-            buttons_list
+            lambda x: types.KeyboardButton(
+                text=emoji.emojize(x),),
+            funcs.keys()
         )
     )
-
-    # Добавляем их в markup
     list(map(lambda x: markup.add(x), btns))
-    # Отправляем
-    bot.send_message(id, "Меню", reply_markup=markup)
+    bot.send_message(chat_id, "Меню", reply_markup=markup)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    id = message.json["chat"]["id"]
-    chat.append(id)
-    bot.send_message(id, "Привет! Это бот помогающий в подготовке к SAT")
-    menu(id)
+    chat_id = message.json["chat"]["id"]
+    bot.send_message(chat_id, "Привет! Это бот помогающий в подготовке к SAT")
+    menu(chat_id)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "menu")
 def menu_calling(call):
-    id = call.message.json["chat"]["id"]
-    menu(id)
+    chat_id = call.message.json["chat"]["id"]
+    menu(chat_id)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "words")
-def new_word(call):
-    id = call.message.json["chat"]["id"]
-    bot.send_message(id, "Выберите количетсво ")
+@bot.message_handler(func=lambda message: emoji.demojize(message.text, use_aliases=True) in funcs.keys())
+def calling(message):
+    print(message.text)
+    message_type = funcs[emoji.demojize(message.text, use_aliases=True)]
+    if message_type == "books":
+        books(message)
+    elif message_type == "officials":
+        officials(message)
+    elif message_type == "tests":
+        print("tests")
+    elif message_type == "words":
+        new_word(message)
+    elif message_type == "what":
+        print("what")
+    elif message_type == "about":
+        print('about')
+
+
+@bot.message_handler(func=lambda message: message.text == "words")
+def new_word(message):
+    chat_id = message.json["chat"]["id"]
+    bot.send_message(chat_id, "Выберите количетсво ")
     numbers = list(range(500))
     answers = []
     markup = types.InlineKeyboardMarkup()
@@ -99,62 +112,54 @@ def new_word(call):
         btn = types.InlineKeyboardButton(text=answers[num], callback_data=answers[num] + " ! " + ques)
         markup.add(btn)
         opt.remove(num)
-    bot.send_message(id, ques, reply_markup=markup)
+    bot.send_message(chat_id, ques, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: (call.data.split(" ! "))[0] in meanings)
 def checking(call):
-    id = call.message.json["chat"]["id"]
+    chat_id = call.message.json["chat"]["id"]
     if words[meanings.index(call.data.split(" ! ")[0])]["word"] == call.data.split(" ! ")[1]:
-        bot.send_message(id, "correct")
+        bot.send_message(chat_id, "correct")
     else:
-        bot.send_message(id, "incorrect")
-
-
-def find_int(s):
-    int_s = ""
-    for c in s:
-        if '0' <= c <= '9':
-            int_s += c
-    return int(int_s)
+        bot.send_message(chat_id, "incorrect")
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "books")
 def books(call):
-    id = call.message.json["chat"]["id"]
+    chat_id = call.json["chat"]["id"]
     markup = types.InlineKeyboardMarkup()
     for book in bookslist:
         btn_book = types.InlineKeyboardButton(text=book["text"], callback_data=book["text"])
         markup.add(btn_book)
-    bot.send_message(id, "Книги", reply_markup=markup)
+    bot.send_message(chat_id, emoji.emojize("Книги для подготовки к SAT :exclamation:", use_aliases=True), reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data in booktextlist.keys())
 def send_books(call):
-    id = call.message.json["chat"]["id"]
+    chat_id = call.message.json["chat"]["id"]
     book = bookslist[booktextlist[call.data]]
     try:
         if book["image_id"]:
-            bot.send_photo(id, book["image_id"])
+            bot.send_photo(chat_id, book["image_id"])
     except:
         pass
-    bot.send_document(id, book["id"])
+    bot.send_document(chat_id, book["id"])
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "officials")
 def officials(call):
-    id = call.message.json["chat"]["id"]
+    chat_id = call.json["chat"]["id"]
     markup = types.InlineKeyboardMarkup()
     for offical in officialslist:
         btn_book = types.InlineKeyboardButton(text=offical["text"], callback_data=offical["text"])
         markup.add(btn_book)
-    bot.send_message(id, "Прошедшие SAT", reply_markup=markup)
+    bot.send_message(chat_id, "Прошедшие SAT", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data in officialstextlist.keys())
 def send_books(call):
-    id = call.message.json["chat"]["id"]
-    bot.send_document(id, officialslist[officialstextlist[call.data]]["id"])
+    chat_id = call.message.json["chat"]["id"]
+    bot.send_document(chat_id, officialslist[officialstextlist[call.data]]["id"])
 
 
 @bot.message_handler(content_types=['document'])
@@ -165,4 +170,23 @@ def add_book(message):
     print(bookslist[-1])
 
 
-bot.polling()
+@bot.message_handler(commands=["everyday"])
+def everyday(message):
+    chat_id = message.json["chat"]["id"]
+    sec = get_current_seconds() + 10
+    smth = {
+        "sender_id": chat_id,
+        "time": sec
+    }
+    try:
+        queue.put_nowait(smth)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    p = Process(target=start_sender, args=(queue,))
+    p.start()
+
+    bot.polling()
+    p.join()
--- sender.py
+++ sender.py
@@ -1,12 +1,14 @@
 import telebot
 import time
 import datetime as date
+import json
+import random
 from multiprocessing import Process, Queue
 
 
 senders = [
-#    {
-#       "sender": sender_id,
+#   {
+#   "sender": sender_id,
 #       "time": time_to_send
 #     }
 ] # must be sorted
@@ -15,43 +17,55 @@ sent = [
 #   sender_id
 ]
 
+def open_json(s):
+    with open(s) as f:
+        return json.load(f)
+
+
+words = open_json("words.json")
+
 number_senderP = 4
 
 
 def get_time(t=None):
-    t = time.time() if t == None else t
+    t = time.time() if t is None else t
     struct_time = time.gmtime(t + 6 * 60 * 60)
     return time.strftime("%d-%m-%Y: %Hh %Mm %Ss", struct_time)
 
 
 def get_current_seconds():
-    now = date.datetime.now().timestamp()
-    today = date.datetime.today().timestamp()
-    return today - now
+    now = date.datetime.now()
+    today = date.datetime(now.year, now.month, now.day)
+    return now.timestamp() - today.timestamp()
 
 
 def get_new_message():
-    # срочно TODO
-    return
+    word = random.choice(words)
+    return "Word of the Day: " + word["word"] + " - " + word["meaning"]
 
 
-def send(bot: telebot.TeleBot, input: Queue):
+def send(input: Queue):
+    TOKEN = "881872217:AAHjDOnnKDXr5D5LnyphqkO7Roe4QboXhTA"
+    bot = telebot.TeleBot(TOKEN)
     while True:
         try:
-            data = input.get_nowait()
-            print(get_time() + f" sending to { data }")
-            bot.send_message(data, get_new_message())
-        except Queue.Empty:
+            sender_id = input.get_nowait()
+            print(get_time() + f" sending to { sender_id }")
+            bot.send_message(sender_id, get_new_message())
+        except Exception:
             pass
 
 
-def start_sender(bot: telebot.TeleBot, input: Queue, output: Queue):
+def start_sender(input):
     global senders
     global sent
 
+    output = Queue(maxsize=100)
+
     senders_processes = []
     for i in range(number_senderP):
-        new_p = Process(target=send, args=(bot, output))
+        new_p = Process(target=send, args=(output,))
+        new_p.start()
         senders_processes.append(new_p) 
     
     while True:
@@ -64,18 +78,20 @@ def start_sender(bot: telebot.TeleBot, input: Queue, output: Queue):
                     "time": data["time"]
                 }
             )
-            senders = sorted(senders, key=lambda x: x["time"])
-        except Queue.Empty:
+            senders = list(sorted(senders, key=lambda x: x["time"]))
+        except Exception:
             pass
+        current = get_current_seconds()
         for s in senders:
-            id_ = s["sender"]
+            sender_id = s["sender"]
             time_ = s["time"]
-            if id_ in sent:
+            if sender_id in sent:
                 continue
-            if time_ > get_current_seconds():
+            if time_ > current:
                 break
-            output.put_nowait(id)
-            sent.append(id_)
+            print(f"start Sending to { sender_id }")
+            output.put_nowait(sender_id)
+            sent.append(sender_id)
 
     for p in senders_processes:
         p.join()
