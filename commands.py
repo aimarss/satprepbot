4a6b01cf108d48860cc0f6bdee6cf50eceaa5132
fuyther <esiredrise@gmail.com>
2020-03-13 23:22:37 +0600

    sender fixed

--- commands.py
+++ commands.py
@@ -215,7 +215,7 @@ class Commands:
             self.othertime(message)
         else:
             user_time = text.split(":")
-            sec = (int(user_time[0]) + timezone) % 24 * 3600 + int(user_time[1]) * 60
+            sec = (int(user_time[0]) - timezone) % 24 * 3600 + int(user_time[1]) * 60
             smth = {
                 "sender_id": chat_id,
                 "time": sec,
